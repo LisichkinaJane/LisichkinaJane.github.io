66a5efb8b9545db5316e9f8b6afc95e39b43e4e4
Jane <Lisichkina.jane@gmail.com>
2017-09-07 12:05:35 +0300

    Commit tabs name

--- bigtopmenu/script/script.js
+++ bigtopmenu/script/script.js
@@ -33,13 +33,13 @@
          }
          return Tabs;
      })();
-     var tableTabs = new Tabs('.btm-li1'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li2'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li3'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li4'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li5'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li6'); //name container which contains the necessary elements
-      var tableTabs = new Tabs('.btm-li7'); //name container which contains the necessary elements
+     var tableTabs1 = new Tabs('.btm-li1'); //name container which contains the necessary elements
+      var tableTabs2 = new Tabs('.btm-li2'); //name container which contains the necessary elements
+      var tableTabs3 = new Tabs('.btm-li3'); //name container which contains the necessary elements
+      var tableTabs4 = new Tabs('.btm-li4'); //name container which contains the necessary elements
+      var tableTabs5 = new Tabs('.btm-li5'); //name container which contains the necessary elements
+      var tableTabs6 = new Tabs('.btm-li6'); //name container which contains the necessary elements
+      var tableTabs7 = new Tabs('.btm-li7'); //name container which contains the necessary elements
 
  });  
     
