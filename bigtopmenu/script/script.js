1659f89d1727399be9e1f103155dfeaf06637fe4
Jane <Lisichkina.jane@gmail.com>
2017-09-07 11:33:06 +0300

    Commit Bigtopmenu
    Menu top with tab JQuery and css

---
+++ bigtopmenu/script/script.js
@@ -0,0 +1,46 @@
+/* Create BigTabMenu by Lisichkina Jane 10.10.2015 */
+ $(document).ready(function () {
+
+     var Tabs = (function () {
+         function Tabs(container) {
+             this.container = $(container);
+             this.controls = $(container).find('.tab-li');
+             this.wrappers = $(container).find('.btm-content-block');
+             this.init();
+         }
+
+         Tabs.prototype.init = function () {
+             var self = this;
+
+             $(this.wrappers).each(function (index) {
+                 (index == 0) ? $(this).show(): $(this).hide();
+             });
+
+               this.controls.on('mouseenter', function () {
+                 $(this)
+                     .addClass('active')
+                     .siblings()
+                     .removeClass('active');
+
+                 var i = $(self.controls).index(this);
+                 $(self.wrappers).each(function (index) {
+                     (index == i) ? $(this).show(): $(this).hide();
+
+                 });
+
+             });
+
+         }
+         return Tabs;
+     })();
+     var tableTabs = new Tabs('.btm-li1'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li2'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li3'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li4'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li5'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li6'); //name container which contains the necessary elements
+      var tableTabs = new Tabs('.btm-li7'); //name container which contains the necessary elements
+
+ });  
+    
+    
